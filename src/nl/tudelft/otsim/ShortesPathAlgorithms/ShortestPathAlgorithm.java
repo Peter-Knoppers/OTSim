5dc8d2527e363310395518d69081a09553557738
Peter Knoppers <P.Knoppers@tudelft.nl>
2013-10-24 10:46:06 +0200

    Heavily edited abstract class ShortestPathAlgorithms

--- src/nl/tudelft/otsim/ShortesPathAlgorithms/ShortestPathAlgorithm.java
+++ src/nl/tudelft/otsim/ShortesPathAlgorithms/ShortestPathAlgorithm.java
@@ -2,46 +2,58 @@ package nl.tudelft.otsim.ShortesPathAlgorithms;
 
 import java.util.ArrayList;
 
+import nl.tudelft.otsim.GeoObjects.Network;
 import nl.tudelft.otsim.GeoObjects.Node;
 
 /**
  * General shape of all Shortest path algorithm implementations
  * 
+ * The methods in this class are supposed to be used like this:
+ * <br />1: call ShortestPathAlgorithm (network) to create an instance of this class.
+ * <br />2: call execute (startNode, endNode) to set the start and end nodes.
+ * <br />3: call hasNext to verify that a path exist (if none exists go to step 6, or go to step 2)
+ * <br />4: call getCost and getPath to obtain the properties of the path
+ * <br />5: call hasNext to check if another path exists (if none exists go to step 6, or go to step 2; otherwise go to step 4)
+ * <br />6: dispose this instance of ShortedPathAlgorithm
+ * 
  * @author Peter Knoppers
  */
 public abstract class ShortestPathAlgorithm {
+	@SuppressWarnings("unused")
+	private Network network;	// will be used in the implementation of execute
+	
 	/**
-	 * Find lowest cost path from all {@link Node Nodes} to the specified end node
+	 * Create an instance of the ShortestPathAlgorithm
+	 * @param network {@link Network}; the Network in which paths must be found
+	 */
+	public ShortestPathAlgorithm (Network network) {
+		this.network = network;
+	}
+	
+	/**
+	 * Find lowest cost paths between two {@link Node Nodes}.
+	 * @param startNode {@link Node}; starting point of all paths
 	 * @param endNode {@Link Node}; end point of all paths
 	 */
-	public abstract void execute (Node endNode);
+	public abstract void execute (Node startNode, Node endNode);
+	
+	/**
+	 * Report whether there is a (further) path and (if there is a next path)
+	 * make that the <i>current</i> path for calls to getCost and getPath.
+	 * @return Boolean; true if there is a(nother) path; false if there is not
+	 */
+	public abstract boolean hasNext();
 	
 	/**
 	 * Retrieve the cost of travel from the specified {@link Node startNode} to the endNode.
-	 * @param startNode {@link Node}; the start node of the trip
 	 * @return Double; the cost of a trip from the specified startNode to the endNode
 	 */
-	public abstract double getLowestCost (Node startNode);
+	public abstract double getCost ();
 	
 	/**
 	 * Retrieve the lowest cost path from {@link Node startNode} to the endNode.
-	 * @param startNode {@link Node}; the start node of the trip
 	 * @return ArrayList&lt;{@link Node}&gt;; the list of nodes visited in the lowest cost path
 	 */
-	public abstract ArrayList<Node> getPath (Node startNode);
-	
-	/**
-	 * Add a penalty to a specified path. All penalties are reset when the 
-	 * execute method is called or the clearPenalties method is called.
-	 * @param ratio Double; fraction of the current cost to add to each path element
-	 * @param perPathElement Double; fixed value to add to the cost of each path element
-	 * @param path ArrayList&lt;{@link Node}&gt;; list of nodes of the path
-	 */
-	public abstract void addPenalty (double ratio, double perPathElement, ArrayList<Node> path);
-	
-	/**
-	 * Clear all penalties added by the addPenalty method.
-	 */
-	public abstract void clearPenalties ();
-	
+	public abstract ArrayList<Node> getPath ();
+		
 }
