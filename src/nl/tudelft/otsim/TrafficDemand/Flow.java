96e08742435a281b6e089b44f809994ecb10fea4
Peter Knoppers <P.Knoppers@tudelft.nl>
2013-11-05 16:40:03 +0100

    Initial version of Flow class (with JUnit testing)

---
+++ src/nl/tudelft/otsim/TrafficDemand/Flow.java
@@ -0,0 +1,200 @@
+package nl.tudelft.otsim.TrafficDemand;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import nl.tudelft.otsim.FileIO.ParsedNode;
+import nl.tudelft.otsim.FileIO.StaXWriter;
+import nl.tudelft.otsim.FileIO.XML_IO;
+import nl.tudelft.otsim.GeoObjects.Network;
+
+/**
+ * This class holds a list of time/flow pairs and allows retrieval of 
+ * (interpolated) flow values for any time.
+ * 
+ * @author Peter Knoppers
+ */
+public class Flow implements XML_IO {
+	/** Tag of a Flow when stored in XML */
+	public static final String XMLTAG = "FlowSet";
+	private static final String XML_TIMEFLOWPAIR = "Set";
+	private static final String XML_TIME = "Time";
+	private static final String XML_FLOW = "Flow";
+	private ArrayList<Double> times = new ArrayList<Double>();
+	private ArrayList<Double> flows = new ArrayList<Double>();
+	private final Network network;
+	private final static String format = "%.3f"; 
+	
+	/**
+	 * Create an empty instance of Flow.
+	 * @param network {@link Network}; the Network that will own this Flow (may be null)
+	 */
+	public Flow(Network network) {
+		this.network = network;
+	}
+	
+	/**
+	 * Create a Flow from an XML description
+	 * @param network {@link Network}; the Network that will own this flow (may be null)
+	 * @param pn {@link ParsedNode} XML node of the Flow object 
+	 * @throws Exception
+	 */
+	public Flow(Network network, ParsedNode pn) throws Exception {
+		this(network);
+		for (int index = 0; index < pn.size(); index++) {
+			ParsedNode subNode = pn.getSubNode(XML_TIMEFLOWPAIR, index);
+			for (String fieldName : subNode.getKeys()) {
+				if (fieldName.equals(XML_TIMEFLOWPAIR)) {
+					for (int i = 0; i < subNode.size(fieldName); i++) {
+						double time = Double.NaN;
+						double flow = Double.NaN;
+						ParsedNode valueNode = subNode.getSubNode(XML_TIME, 0);
+						if (null != valueNode)
+							time = Double.parseDouble(valueNode.getValue());
+						valueNode = subNode.getSubNode(XML_FLOW, 0);
+						if (null != valueNode)
+							flow = Double.parseDouble(valueNode.getValue());
+						if (Double.isNaN(time) || Double.isNaN(flow))
+							throw new Exception("incompletely defined time/flow pair near " + subNode.description());
+						insertPair(time, flow);
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Insert a time/flow pair.
+	 * <br /> If several values are inserted with the exact same time, the 
+	 * order of the stored time/flow pairs is undefined.
+	 * @param time Double; the time (in s relative to simulation start time)
+	 * @param flow Double; the flow in vehicles / hour
+	 */
+	public void insertPair (double time, double flow) {
+		int position;
+		for (position = 0; (position < times.size()) && (time > times.get(position)); position++)
+			;
+		times.add(position, time);
+		flows.add(position, flow);
+		if (null != network)
+			network.setModified();
+	}
+	
+	/**
+	 * Retrieve the number of time/flow pairs stored.
+	 * @return Integer; the number of time/flow pairs stored.
+	 */
+	public int size() {
+		return times.size();
+	}
+	
+	/**
+	 * Retrieve the time of a particular time/flow pair.
+	 * @param index Integer; the index of the time/flow pair
+	 * @return Double; the time of the selected time/flow pair
+	 */
+	public double getTime(int index) {
+		return times.get(index);
+	}
+	
+	/**
+	 * Retrieve the flow of a particular time/flow pair.
+	 * @param index Integer; the index of the time/flow pair
+	 * @return Double; the flow of the selected time/flow pair
+	 */
+	public double getFlow(int index) {
+		return flows.get(index);
+	}
+
+	/**
+	 * Remove a time/flow pair.
+	 * @param index Integer; the index of the time/flow pair to remove
+	 */
+	public void deletePair (int index) {
+		times.remove(index);
+		flows.remove(index);
+		if (null != network)
+			network.setModified();
+	}
+	
+	/**
+	 * Retrieve the flow value at a specified time.
+	 * <br />Before the first specified time and after the last specified time
+	 * the returned flow value remains constant.
+	 * @param time Double; the time in s relative to simulation start time
+	 * @return Double; the (interpolated) flow in vehicles / hour at the specified time 
+	 */
+	public double getFlow(double time) {
+		if (flows.size() == 0)
+			throw new Error("undefined flow");
+		double prevTime = 0;
+		double prevFlow = flows.get(0);
+		for (int i = 0; i < times.size(); i++) {
+			Double thisTime = times.get(i);
+			Double thisFlow = flows.get(i);
+			if (thisTime <= time) {
+				prevTime = thisTime;
+				prevFlow = thisFlow;
+			} else if (thisTime > prevTime )
+				return prevFlow + (thisFlow - prevFlow) * (time - prevTime) / (thisTime - prevTime);
+			else
+				return thisFlow;
+		}
+		return prevFlow;
+	}
+
+	private boolean writePairs(StaXWriter staXWriter) {
+		for (int index = 0; index < size(); index++) {
+			if (! staXWriter.writeNodeStart(XML_TIMEFLOWPAIR))
+				return false;
+			if (! staXWriter.writeNode(XML_TIME, String.format(Locale.US, format, getTime(index))))
+				return false;
+			if (! staXWriter.writeNode(XML_FLOW, String.format(Locale.US, format, getFlow(index))))
+				return false;
+			if (! staXWriter.writeNodeEnd(XML_TIMEFLOWPAIR))
+				return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean writeXML(StaXWriter staXWriter) {
+		return staXWriter.writeNodeStart(XMLTAG)
+				&& writePairs(staXWriter)
+				&& staXWriter.writeNodeEnd(XMLTAG);
+	}
+	
+	/**
+	 * Export this Flow in a textual format.
+	 * @return String; this Flow in a textual format
+	 */
+	public String export () {
+		String result = "";
+		final String formatPair = format + "/" + format;
+		for (int i = 0; i < size(); i++) {
+			double time = times.get(i);
+			double flow = flows.get(i);
+			//if ((time > 0) && (result.length() == 0))	// Insert initial value for convenience
+			//	result = String.format(Locale.US, formatPair, time, flow);
+			result += String.format(Locale.US, "%s" + formatPair, result.length() > 0 ? "\t" : "", time, flow);
+		}
+		return result;
+	}
+	
+	/**
+	 * Create a Flow from a textual description with no owning Network.
+	 * <br /> This can be useful in {@link Simulators}.
+	 * @param description
+	 */
+	public Flow (String description) {
+		network = null;
+		String pairs[] = description.split("\t");
+		for(String pair : pairs) {
+			String fields[] = pair.split("/");
+			double time = Double.parseDouble(fields[0]);
+			double flow = Double.parseDouble(fields[1]);
+			insertPair(time, flow);
+		}
+	}
+}
+
---
+++ src/nl/tudelft/otsim/TrafficDemand/FlowTest.java
@@ -0,0 +1,197 @@
+package nl.tudelft.otsim.TrafficDemand;
+
+import static org.junit.Assert.*;
+
+import nl.tudelft.otsim.GeoObjects.Network;
+
+import org.junit.Test;
+
+public class FlowTest {
+
+	@Test
+	public void testFlowNetwork() {
+		Network n = new Network();
+		n.clearModified();
+		Flow f = new Flow(n);
+		f.insertPair(10, 10);
+		assertTrue("Inserting a pair must set the modified flag in the network", n.isModified());
+		f = new Flow((Network) null);
+		try {
+			f.insertPair(10, 20);	// should NOT try to set the modified flag in the null-network
+		} catch (Exception e) {
+			fail("If network is null, setting the modified flag should not be attempted");
+		}		
+	}
+
+	@Test
+	public void testFlowNetworkParsedNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertPair() {
+		Flow f = new Flow((Network) null);
+		f.insertPair(10,  20);
+		assertEquals("Single entry time value can be retrieved", f.getTime(0), 10, 0.000001);
+		assertEquals("Single entry flow value can be retrieved", f.getFlow(0), 20, 0.000001);
+	}
+
+	@Test
+	public void testSize() {
+		Flow f = new Flow((Network) null);
+		for (int i = 0; i < 100; i++) {
+			assertEquals("Number of inserted values should match number in insertPair calls", f.size(), i);
+			f.insertPair(10 * i, 5 + 20 * i);
+		}
+	}
+
+	@Test
+	public void testGetTime() {
+		Flow f = new Flow((Network) null);
+		for (int i = 0; i < 100; i++)
+			f.insertPair(10 * i, 5 + 20 * i);
+		for (int i = 0; i < 100; i++)
+			assertEquals("Time values can be retrieved and are correct", f.getTime(i), 10 * i, 0.0001);
+		boolean exceptionThrown = false;
+		try {
+			f.getTime(-1);
+		} catch (Exception e) {
+			exceptionThrown = true;
+		}
+		assertTrue("Negative index is not permitted in getTime", exceptionThrown);
+		exceptionThrown = false;
+		try {
+			f.getTime(100);
+		} catch (Exception e) {
+			exceptionThrown = true;
+		}
+		assertTrue("Too large index is not permitted in getTime", exceptionThrown);
+	}
+
+	@Test
+	public void testGetFlowInt() {
+		Flow f = new Flow((Network) null);
+		for (int i = 0; i < 100; i++)
+			f.insertPair(10 * i, 5 + 20 * i);
+		for (int i = 0; i < 100; i++)
+			assertEquals("Flow values can be retrieved and are correct", f.getFlow(i), 5 + 20 * i, 0.0001);
+		boolean exceptionThrown = false;
+		try {
+			f.getFlow(-1);
+		} catch (Exception e) {
+			exceptionThrown = true;
+		}
+		assertTrue("Negative index is not permitted in getFlow", exceptionThrown);
+		exceptionThrown = false;
+		try {
+			f.getFlow(100);
+		} catch (Exception e) {
+			exceptionThrown = true;
+		}
+		assertTrue("Too large index is not permitted in getFlow", exceptionThrown);
+	}
+
+	@Test
+	public void testDeletePair() {
+		Flow f = new Flow((Network) null);
+		for (int i = 0; i < 100; i++)
+			f.insertPair(10 * i, 5 + 20 * i);
+		boolean exceptionThrown = false;
+		try {
+			f.deletePair(-1);
+		} catch (Exception e) {
+			exceptionThrown = true;
+		}
+		assertTrue("Index may not be negative", exceptionThrown);
+		exceptionThrown = false;
+		try {
+			f.deletePair(101);
+		} catch (Exception e) {
+			exceptionThrown = true;
+		}
+		assertTrue("Index must not exceed number of entries", exceptionThrown);
+		int index = 0;
+		int indexIncrement = 33;
+		for (int i = 100; --i > 0; ) {
+			assertEquals("Removing a (semi-random) entry reduces the number of entries by one", f.size(), i + 1);
+			index %= f.size();
+			f.deletePair(index);
+			index += indexIncrement;
+		}
+		assertEquals("After removing all but one entries there should be one entry", f.size(), 1);
+		f.deletePair(0);
+		assertEquals("After removing all entries there should be none left", f.size(), 0);
+		exceptionThrown = false;
+		try {
+			f.deletePair(0);
+		} catch (Exception e) {
+			exceptionThrown = true;
+		}
+		assertTrue("calling remove on empty set throws exception", exceptionThrown);
+		
+	}
+
+	@Test
+	public void testGetFlowDouble() {
+		Flow f = new Flow((Network) null);
+		boolean exceptionThrown = false;
+		try {
+			f.getFlow(0d);
+		} catch (Error e) {
+			exceptionThrown = true;
+		}
+		assertTrue("Trying to get a flow value from an empty list throws exception", exceptionThrown);
+		f.insertPair(10,  20);
+		assertEquals("Only one value results in a uniform flow", f.getFlow(0d), 20, 0.00001);
+		assertEquals("Only one value results in a uniform flow", f.getFlow(100d), 20, 0.00001);
+		assertEquals("Only one value results in a uniform flow; even for negative times", f.getFlow(-100d), 20, 0.00001);
+		f.insertPair(40, 100);
+		f.insertPair(70, 50);
+		double t = 0;
+		for ( ; t < 10.1; t += 0.5)
+			assertEquals("Flow is constant up to first time value", f.getFlow(t), 20, 000001);
+		for ( ; t < 40.1; t += 0.5)
+			assertEquals("Flow changes linearly between time values (1)", f.getFlow(t), 20 + (t - 10) * (100 - 20) / 30, 0.00001);
+		for ( ; t < 70.1; t += 0.5)
+			assertEquals("Flow changes linearly between time values (2)", f.getFlow(t), 100 + (t - 40) * (50 - 100) / 30, 0.000001);
+		for ( ; t < 100.1; t += 0.5) 
+			assertEquals("Flow stays constant after last time value", f.getFlow(t), 50, 0.000001);
+	}
+
+	@Test
+	public void testWriteXML() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExport() {
+		Flow f = new Flow((Network) null);
+		assertEquals("empty Flow exports as empty string", f.export().length(), 0);
+		f.insertPair(20, 10d / 30);
+		assertEquals("check value and number of decimal digits", f.export(), "20.000/0.333");
+		f.insertPair(30,  40);
+		assertEquals("check single tab char between entries", f.export(), "20.000/0.333\t30.000/40.000");
+	}
+
+	@Test
+	public void testFlowString() {
+		Flow f = new Flow((Network) null);
+		for (int i = 0; i < 5; i++) {
+			// Generate values with 3 decimal digits (which is the guaranteed precision)
+			double time = Math.round(i * 10000000d / 333) / 1000d;
+			double flow = Math.round(1000000 - i * 200000000d / 987) / 1000d;
+			f.insertPair(time, flow);
+		}
+		System.out.println("export is \"" + f.export() + "\"");
+		Flow f2 = new Flow(f.export());
+		assertEquals("Number of pairs in copy should be the same", f.size(), f2.size());
+		for (int i = 0; i < 5; i++) {
+			assertEquals("There should be no rounding error due to conversion to text and back", f.getTime(i), f2.getTime(i), 0.0000001);
+			assertEquals("There should be no rounding error due to conversion to text and back", f.getFlow(i), f2.getFlow(i), 0.0000001);
+		}
+		// Now test that values between the pairs are also close enough
+		for (double t = 0; t < 150.1; t += 0.5)
+			assertEquals("There should be no significant rounding error due to the time/flow pairs being (virtually) identical", f.getFlow(t), f2.getFlow(t), 0.0000001);
+	}
+
+}
